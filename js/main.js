d7d5b8e1cf3b86776318bc1360cb35e3b2098182
Ishan <ishanbagchi4@gmail.com>
2021-05-18 15:22:48 +0530

    configure section windows

--- js/main.js
+++ js/main.js
@@ -6,51 +6,53 @@ let aboutCounter = 0
 let contactCounter = 0
 
 about.addEventListener('click', () => {
-    aboutCounter = aboutCounter + 1
-    if (aboutCounter == 1) {
-        const aboutBox = new WinBox({
-            title: 'About Me',
-            width: '300px',
-            height: '400px',
-            top: 50,
-            right: 50,
-            bottom: 50,
-            left: 50,
-            mount: aboutContent.cloneNode(true),
-            onfocus: function() {
-                this.setBackground('#00aa00')
-            },
-            onblur: function() {
-                this.setBackground('#777')
-            },
-            onclose: function() {
-                aboutCounter = 0
-            }
-        })
-    }
+	aboutCounter = aboutCounter + 1
+	if (aboutCounter == 1) {
+		const aboutBox = new WinBox({
+			title: 'About Me',
+			width: '300px',
+			height: '450px',
+			border: '0.1em',
+			top: 50,
+			right: 50,
+			bottom: 0,
+			left: 50,
+			mount: aboutContent.cloneNode(true),
+			onfocus: function () {
+				this.setBackground('#00aa00')
+			},
+			onblur: function () {
+				this.setBackground('#777')
+			},
+			onclose: function () {
+				aboutCounter = 0
+			},
+		})
+	}
 })
 
 contact.addEventListener('click', () => {
-    contactCounter = contactCounter + 1
-    if (contactCounter == 1) {
-        const contactBox = new WinBox({
-            title: 'Contact Me',
-            width: '300px',
-            height: '400px',
-            top: 50,
-            right: 50,
-            bottom: 50,
-            left: 50,
-            mount: contactContent.cloneNode(true),
-            onfocus: function() {
-                this.setBackground('#00aa00')
-            },
-            onblur: function() {
-                this.setBackground('#777')
-            },
-            onclose: function() {
-                contactCounter = 0
-            }
-        })
-    }
+	contactCounter = contactCounter + 1
+	if (contactCounter == 1) {
+		const contactBox = new WinBox({
+			title: 'Contact Me',
+			width: '300px',
+			height: '400px',
+			border: '0.1em',
+			top: 50,
+			right: 50,
+			bottom: 0,
+			left: 50,
+			mount: contactContent.cloneNode(true),
+			onfocus: function () {
+				this.setBackground('#00aa00')
+			},
+			onblur: function () {
+				this.setBackground('#777')
+			},
+			onclose: function () {
+				contactCounter = 0
+			},
+		})
+	}
 })
